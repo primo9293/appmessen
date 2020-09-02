53ff032399d47288a08a01772e72a91569b20084
primo9293 <42128291+primo9293@users.noreply.github.com>
2020-09-01 20:08:12 -0500

    Inicio componenst

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { ConversationComponent } from './conversation/conversation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    ConversationComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
